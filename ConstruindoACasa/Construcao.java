2d17a2bc505d253f6dac668cb57694cf8652bd11
mayconr4 <mayconhs2007@gmail.com>
2025-04-29 10:58:04 -0300

    Add files via upload

---
+++ ConstruindoACasa/Construcao.java
@@ -0,0 +1,12 @@
+package ConstruindoACasa;
+
+public class Construcao {
+
+	public static void main(String[] args) {
+		MaoDeObra gesseiro = new MaoDeObra();   // ctrl + barra de espaço atalho para achar classes
+		 gesseiro.gesseiro();
+		
+		
+	}
+
+}
---
+++ ConstruindoACasa/MaoDeObra.java
@@ -0,0 +1,32 @@
+package ConstruindoACasa;
+
+public class MaoDeObra {   	
+	
+	public  static void pedreiro() { 
+		System.out.println("Estou Contruindo");
+		
+	}  	
+	public static void azulegista() { 
+		System.out.println("Estou colocando o azuleijo"); 		
+		
+	}
+	public static void eletricista() { 
+		System.out.println("Estou colocando a afiação"); 		
+		
+	}
+	public static void pintor() { 
+		System.out.println("Estou pintando"); 		
+		
+	} 	
+	public static void marmoreiro() { 
+		System.out.println("Estou colocando marmore"); 		
+		
+	}
+	public void gesseiro() { 
+		System.out.println("Estou colocando o gesso");
+		System.out.println("Terminei de colocar o gesso");
+		
+	}
+	
+	
+}
