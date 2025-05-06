cbea7db7f7fcade4e4f77c4086b01422ab83a678
mayconr4 <mayconhs2007@gmail.com>
2025-05-06 11:19:32 -0300

    Add files via upload

---
+++ Fase_02/Desafio_01.java
@@ -0,0 +1,99 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Desafio_01 {
+
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+
+        double nivel_escolhido,desconto;
+        int nivel, diaDePagar ;
+
+        System.out.println("digite o seu nivel de mensalidade: ");
+        nivel = ler.nextInt();
+
+        nivel_escolhido = descobrirNivel(nivel);
+        //System.out.println(nivel_escolhido);
+
+        System.out.println("digite o dia que voce planeja pagar: ");
+        diaDePagar = ler.nextInt();
+
+        desconto = descobrirPagamento_desconto(diaDePagar,nivel_escolhido);
+
+
+
+    }//Classe para  solictar estes métodos
+
+        public static double descobrirNivel(int opcao){ //METODO
+        double teste;
+       // int nivel_de_mensalidade;
+
+        Scanner ler = new Scanner(System.in);
+        /*System.out.println("Digite o seu nivel de mensalidade: ");
+        nivel_de_mensalidade = ler.nextInt();*/
+
+
+        switch (opcao) {
+            case 1:
+
+                teste = 51.50;
+                System.out.println(teste);
+                break;
+            case 2:
+
+                teste = 65.00;
+                System.out.println(teste);
+                break;
+            case 3:
+
+                teste = 80.00;
+                System.out.println(teste);
+                break;
+            case 4:
+
+                teste = 100.00;
+                System.out.println(teste);
+                break;
+            default:
+                teste = 0.00;
+               System.out.println("tente noamente Nivel inválido: ");
+        }
+    return teste;
+    } //metodo descobrirnivel
+
+        public  static double descobrirPagamento_desconto(int diaDepagamento, double nivelEscolhido){
+            double   conta = nivelEscolhido;
+
+
+        switch (diaDepagamento){
+
+            case 1,2,3,4:
+
+            conta = nivelEscolhido*(1- 15.0/100);
+                System.out.println("voce teve 15% de desconto: "+conta);
+
+
+            break;
+            case 5,6,7,8,9:
+
+            conta =  nivelEscolhido*(1- 10.0/100);
+                System.out.println("voce teve 10% de desconto: "+conta);
+
+            break;
+            case 10:
+
+            conta = nivelEscolhido*(1- 3.89/100);
+                System.out.println("Voce teve 3,89% de desconto: "+conta); //R$%.2f%n arredondar numero
+
+            break;
+            default:
+                System.out.println("Sem desconto: "+conta);
+
+        }
+            return conta;
+        }//Metodo DescobrirPpagamento_deconto
+
+
+}//CLASE CENTRAL
+
+
---
+++ Fase_02/Desafio_02.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Desafio_02 {
+
+    public static void main(String[] args){
+        String escolher_gasolina;
+        double receberGasolina;
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Escolha uma gasolina: ");
+        System.out.println("gasolina comun ");
+        System.out.println("alcool: ");
+        System.out.println("diesel: ");
+        System.out.println("gasolina aditivada: ");
+        System.out.println("Digite a opcçao desejada: ");
+        escolher_gasolina = ler.nextLine();
+
+        receberGasolina = descobrirGasolina(escolher_gasolina);
+        System.out.println("custa"+receberGasolina+"R$");
+
+
+
+    }
+
+    public static double descobrirGasolina(String nomeGasolina ){
+        double acumuladora ; // iniciando acumuladora por conta do return
+
+        switch (nomeGasolina){
+
+            case"gasolina comum":
+
+                acumuladora = 2.992;
+                //System.out.println(acumuladora+"R$");
+            break;
+            case"álcool":
+
+                acumuladora = 2.1095;
+            break;
+            case"diesel":
+
+                acumuladora = 2.283;
+            break;
+            case"gasolina aditivada: ":
+
+                acumuladora = 3.054;
+            break;
+            default:
+               acumuladora = 0;
+               System.out.println("Digite novamente: ");
+        }
+        return acumuladora;
+    }
+
+    public static double descobirDesconto(String metodoDepagamento,int dia, double gasolina){
+        double recebeValor  = gasolina;//exemplo de nome qualquer
+
+        switch (metodoDepagamento){
+
+            case "a vista":
+
+             recebeValor = gasolina*(1 - 5.3/100);
+            case "cheque pré-datado":
+
+                if (dia >21  &&  dia<=32){
+
+                    recebeValor = gasolina*(1 + 15.0/100);
+                } else {
+                    double receberValor = gasolina;
+                }
+            break;
+            case "Pagamento normal: ":
+
+                recebeValor = gasolina;
+            break;
+
+            default:
+                System.out.println("Digite novamente: ");
+                recebeValor = gasolina;
+        }
+        return recebeValor;
+    }
+}
